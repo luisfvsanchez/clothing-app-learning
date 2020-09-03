ea8977a4ce3b8a9349f254c04ef25a950e906654
luisfvsanchez <luisfvsanchez@hotmail.com>
2020-09-02 21:52:00 -0300

    Cart icon, cart dropdown and implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 
+import PropTypes from 'prop-types';
+
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import {auth} from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo'></Logo>
@@ -23,13 +27,23 @@ const Header = ({currentUser}) => (
           SIGN OUT</div>) :
         (<Link className='option' to='/signin'>SIGN IN</Link>)
       }
+
+      <CartIcon />
     </div>
+    {
+      hidden ? null : ( <CartDropdown />)
+    }
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden,
 });
 
+Header.propTypes = {
+  currentUser: PropTypes.object,
+  hidden: PropTypes.bool,
+}
 export default connect(mapStateToProps)(Header);
 

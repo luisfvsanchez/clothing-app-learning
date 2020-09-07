83536ef8ab4d1bf75a5f1a0f0cdcfeaf5fa870bc
luisfvsanchez <luisfvsanchez@hotmail.com>
2020-09-07 10:13:49 -0300

    update title and favicon

--- src/components/checkout/checkout.component.jsx
+++ src/components/checkout/checkout.component.jsx
@@ -52,7 +52,7 @@ const CheckoutPage = ({cartItems, totalValue}) => (
     <div className='test-warning'>
       *Please use the following test credit card form payments*
       <br/>
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
     </div>
     <StripeCheckoutButton price={totalValue} />
   </div>
